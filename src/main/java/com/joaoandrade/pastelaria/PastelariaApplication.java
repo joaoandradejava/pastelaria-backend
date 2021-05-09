8170f8038c74e740d60d2f7eb4b0bde1a173732d
joaoandradejava <andradepinheironeto24@hotmail.com>
2021-05-09 02:11:52 -0300

    Primeiro commit

---
+++ src/main/java/com/joaoandrade/pastelaria/PastelariaApplication.java
@@ -0,0 +1,13 @@
+package com.joaoandrade.pastelaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastelariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastelariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaoandrade/pastelaria/PastelariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoandrade.pastelaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastelariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
