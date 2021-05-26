39c307d87c20f24051219a9ede559751844391b8
joaoandradejava <andradepinheironeto24@hotmail.com>
2021-05-26 06:30:14 -0300

    Adicionando cache no retorno dos dados dos produtos mais vendidos

--- src/main/java/com/joaoandrade/pastelaria/api/controller/EstatisticaController.java
+++ src/main/java/com/joaoandrade/pastelaria/api/controller/EstatisticaController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,10 +29,11 @@ public class EstatisticaController {
 	@Operation(summary = "Busca os produtos mais vendidos - ADMIN", description = "Busca os produtos mais vendidos - ADMIN")
 	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/produtos-mais-vendidos")
-	public Page<ProdutoMaisVendidoDTO> buscarOsProdutosMaisVendidos(@PageableDefault(size = 6) Pageable pageable) {
+	public ResponseEntity<Page<ProdutoMaisVendidoDTO>> buscarOsProdutosMaisVendidos(
+			@PageableDefault(size = 6) Pageable pageable) {
 		Page<ProdutoMaisVendidoDTO> page = estatisticaService.buscarOsProdutosMaisVendidos(pageable);
 
-		return page;
+		return ResponseEntity.ok().cacheControl(CacheControl.noCache()).body(page);
 	}
 
 	@Operation(summary = "Gera um relatorio dos produtos e a quantidade de vezes que foram vendidos - ADMIN", description = "Gera um relatorio dos produtos e a quantidade de vezes que foram vendidos - ADMIN")
